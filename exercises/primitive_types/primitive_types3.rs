f70228ffa1b5002cd8784bcaebb8af82d6cd6f7f
boyu <boyu@oyudeMacBook-Air.local>
2023-02-26 22:07:33 +0800

    update

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,10 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    // 声明长度为100的数组
+    let a = [3; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
